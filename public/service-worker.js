b325b27cedb2522ece9ee9507ace3a102cadbcd0
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-08-11 01:56:45 -0500

    small comment inside service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,8 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'
 ];
 
+// something is still wrong with FILES_TO_CACHE, not sure what?
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
